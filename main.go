c827a6a31e410785f9aa5f38115713145f409c2b
Dani Ihza Farrosi <dani.videda@gmail.com>
2025-03-13 17:34:41 +0700

    simple server 2

--- main.go
+++ main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello %s", r.URL.Path[1:])
-}
-
-type server struct {
+type api struct {
 	addr string
 }
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello %s", r.URL.Path[1:])
+func (a *api) handleRequest(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from " + r.URL.Path))
+	log.Default().Printf("%v %v", r.Method, r.URL.Path)
+}
+
+func (a *api) handleGetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from get users"))
+	log.Default().Printf("%v %v", r.Method, r.URL.Path)
 }
 
 func main() {
-	s := &server{addr: ":8080"}
-	fmt.Printf("Server running on addr: %v", s.addr)
-	log.Fatal(http.ListenAndServe(s.addr, s))
+	api := &api{addr: ":8080"}
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", api.handleRequest)
+	mux.HandleFunc("POST /users", api.handleGetUsers)
+	mux.HandleFunc("GET /users", api.handleGetUsers)
+
+	srv := http.Server{
+		Addr:    api.addr,
+		Handler: mux,
+	}
+
+	log.Printf("Listening to port %v\n", api.addr)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
