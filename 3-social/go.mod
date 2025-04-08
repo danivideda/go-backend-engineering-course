2c2b9091652d77d4b52d69bebbd27dfae5d2341f
Dani Ihza Farrosi <dani.videda@gmail.com>
2025-04-08 14:45:00 +0700

    Add hot reloading with air, env with direnv

--- 3-social/go.mod
+++ 3-social/go.mod
@@ -1,3 +1,5 @@
 module github.com/danivideda/go-backend-engineering-course/social
 
 go 1.23.5
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
---
+++ 3-social/internal/env/env.go
@@ -0,0 +1,31 @@
+package env
+
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
+func GetString(key, fallback string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	return val
+}
+
+func GetInt(key string, fallback int) int {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	valAsInt, err := strconv.Atoi(val)
+	if err != nil {
+		log.Fatalf("Integer expected, '%s' given instead", val)
+		return fallback
+	}
+
+	return valAsInt
+}
